784b6751e33233d1f6bba120f5873dbf8e588af2
yxr <2447931063@qq.com>
2019-08-29 00:31:40 +0800

    init project

---
+++ src/main/java/com/yxr/CommunityYxrApplication.java
@@ -0,0 +1,13 @@
+package com.yxr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityYxrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityYxrApplication.class, args);
+    }
+
+}
